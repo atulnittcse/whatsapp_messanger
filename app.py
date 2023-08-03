4b87e6f40463321f550788e6142a1551c6c23c61
Jayant Raj <jayantraj66041@gmail.com>
2023-08-03 14:48:26 +0530

    autopygui make faster

--- app.py
+++ app.py
@@ -18,6 +18,43 @@ def index():
 
     return render_template('index.html')
 
+
+def send_whatsapp_message(group_ids, message):
+    url = f"whatsapp://send?text={message}"
+    pyautogui.hotkey('ctrl', 't')
+    pyautogui.typewrite(url + '\n')
+
+    time.sleep(1)
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    time.sleep(5)
+
+    select_groups(group_ids)
+
+    # send all the selected message
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+
+
+def select_groups(group_ids):
+    is_first = True
+
+    for group in group_ids:
+        pyautogui.typewrite(group)
+        time.sleep(1)
+
+        if is_first:
+            pyautogui.press("tab")
+            pyautogui.press("enter")
+            is_first = False
+
+        else:
+            pyautogui.click(950, 480)
+
+        time.sleep(1)
+
+
+'''
 def send_whatsapp_message(group_ids, message):
     time.sleep(5)
     for group_id in group_ids:
@@ -31,6 +68,7 @@ def open_group_chat(group_id):
     group_url = f'https://web.whatsapp.com/accept?code={group_id}'
     pyautogui.hotkey('ctrl', 't')
     pyautogui.typewrite(group_url + '\n')
+'''
 
 if __name__ == '__main__':
     app.run()
