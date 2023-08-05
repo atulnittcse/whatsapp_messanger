ca9e9fc60be7ae91420645f6e8f9294fb7186d58
abhigyan412 <74041526+abhigyan412@users.noreply.github.com>
2023-08-05 11:15:36 +0530

    w

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def send_whatsapp_message(group_ids, message):
 
 def send_whatsapp_messages(group_ids, message):
       with concurrent.futures.ProcessPoolExecutor() as executor:
-       result = executor.map(send_whatsapp_message, group_ids)
+       result = executor.map(send_whatsapp_message, group_ids , message)
            
 
 def open_group_chat(group_id):
