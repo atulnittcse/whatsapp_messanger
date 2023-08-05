17619217a6ec6969463efd43abafb2cc1279350a
abhigyan412 <74041526+abhigyan412@users.noreply.github.com>
2023-08-05 12:06:11 +0530

    w

--- app.py
+++ app.py
@@ -21,17 +21,17 @@ def index():
 
 
 def send_whatsapp_message(group_ids, message):
-    time.sleep(4)
+    time.sleep(5)
     for group_id in group_ids:
         open_group_chat(group_id)
-        time.sleep(7) 
+        time.sleep(20) 
         pyautogui.typewrite(message)
         pyautogui.press('enter')
         time.sleep(2)  
 
-#def send_whatsapp_messages(group_ids, message):
-      #with concurrent.futures.ProcessPoolExecutor() as executor:
-       #result = executor.map(send_whatsapp_message, group_ids , message)
+def send_whatsapp_messages(group_ids, message):
+      with concurrent.futures.ProcessPoolExecutor() as executor:
+       result = executor.map(send_whatsapp_message, group_ids , message)
            
 
 def open_group_chat(group_id):
